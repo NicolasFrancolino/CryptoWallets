1303a22f6a33119014842fef6ec7fe19489c1ddc
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-09-06 16:17:29 -0300

    Agregar archivos de proyecto.

---
+++ CryptoWallets/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CryptoWallets.Server.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CryptoWallets.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ CryptoWallets/Server/Data/Migrations/20220906190133_primera-migration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CryptoWallets.Server.Data.Migrations
+{
+    public partial class primeramigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
