09c6a26bcd81dce4d00c54529f6459915db1b6f4
Damian <103905478+DamianBosatta@users.noreply.github.com>
2022-09-15 14:17:08 -0300

    nueva clase

---
+++ CryptoWallets/Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoWallets.Shared
+{
+    internal class Class1
+    {
+    }
+}
